2644f52df0103c0911694f639a7d7db9375710ed
Kamphe <Kamphe@KamphePC>
2025-05-06 11:06:48 +0200

    Aggiunto metodo per stampare in output + relativa documentazione tecnica

--- EsempioGIT/src/esempiogit/Programma.java
+++ EsempioGIT/src/esempiogit/Programma.java
@@ -5,13 +5,20 @@ package esempiogit;
  * @author Kamphe
  */
 public class Programma {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello GIT!");
+        var mySelfInstance = new Programma();
+        mySelfInstance.putMessage("Hello GIT!");
     }
-    
+
+    /**
+     * Stampa in output il messaggio dato come parametro
+     * @param msg Messaggio da stampare in output
+     */
+    private void putMessage(String msg) {
+        System.out.println(msg);
+    }
 }
